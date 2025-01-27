d3f9a0721a6a23e8ba754c71ef2726be91fa4e9c
boby87 <fokoutghislain@gmail.com>
2025-01-27 10:45:39 +0400

    authen server

---
+++ authServer/src/test/java/cm/ftg/authServer/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.ftg.authServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
